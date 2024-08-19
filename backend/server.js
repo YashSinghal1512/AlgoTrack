3c262c3dd83411ac7ce8e6b6e207977acfc5a033
Yash Singhal <300yashsinghal@gmail.com>
2024-08-19 21:59:35 +0530

    Update file

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://300yashsinghal:mntFzMj4gjXVW7et@300yashsinghal/?ssl=true&replicaSet=atlas-h3pjqt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0', {
-  //useNewUrlParser: true,
-  //useUnifiedTopology: true
+mongoose.connect('mongodb+srv://300yashsinghal:mntFzMj4gjXVW7et@cluster0.mongodb.net/myDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 const questionSchema = new mongoose.Schema({
